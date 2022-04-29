d272fc76bfe001acd09685de1ea185e6424c2e1f
YufanZheng11 <yufan.zheng1@gmail.com>
2022-04-30 02:36:09 +0800

    optimize 0214

--- 0214_Shortest_Palindrome.py
+++ 0214_Shortest_Palindrome.py
@@ -22,8 +22,6 @@ Constraints:
 s consists of lowercase English letters only.
 """
 
-import cProfile
-
 
 class Solution:
 
@@ -31,34 +29,20 @@ class Solution:
         n = len(s)
         if n <= 1:
             return s
-        shortest = None
-        for i in range(n//2):
-            shortest = self.updateShortest(s, i, i, shortest, n)
-            if s[i] == s[i+1]:
-                shortest = self.updateShortest(s, i, i+1, shortest, n)
-        if n % 2 == 1:
-            shortest = self.updateShortest(s, n//2, n//2, shortest, n)
-        return shortest
+        for right in range(n-1, -1, -1):
+            if self.isPalindrome(s, 0, right):
+                return s[right+1:][::-1] + s
 
-    def updateShortest(self, s, left, right, shortest, n):
-        left, right = self.expandAtCenter(s, left, right, n)
-        if left == 0:
-            current = self.formPalindrome(s, right)
-            if not shortest or len(shortest) > len(current):
-                shortest = current
-        return shortest
-
-    def expandAtCenter(self, s, left, right, n):
-        while left >= 1 and right < n-1 and s[left-1] == s[right+1]:
-            left -= 1
-            right += 1
-        return left, right
-
-    def formPalindrome(self, s, right):
-        return s[right+1:][::-1] + s
+    def isPalindrome(self, s, left, right):
+        while left <= right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
 
 
 if __name__ == '__main__':
     s = Solution()
-    string = "a" * 5000
-    cProfile.run("s.shortestPalindrome(string)")
+    string = "aacecaaa"
+    s.shortestPalindrome(string)
